db2f6b303a4f77a000160071d376741ebde99b1c
AbhinavGalar <abhinavgalar442@gmail.com>
2024-12-12 22:28:54 +0530

    update

---
+++ 12 december/script.js
@@ -0,0 +1,4 @@
+let data="Google is very good company .Google has very much imployees to work in it. google has very high package for employees";
+// I is for case-sensetive and g is for dlobal search
+let mydata= data.replace(/google/ig,"amazon");
+console.log(mydata);
