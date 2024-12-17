a6a2dbc49b67fceb509c5abd89a0f24491a8a259
AbhinavGalar <abhinavgalar442@gmail.com>
2024-12-17 22:43:02 +0530

    dec17

---
+++ decmber 17/script.js
@@ -0,0 +1,19 @@
+let array1=[1,2,3,4,5,6];
+console.log(array1);
+
+let arrray2=["abhinav", "drashti","babu"];
+console.log(arrray2);
+
+const fruits =["banana","pineapple","apple","mango","guava"];
+console.log(fruits);
+const mixed=[1,2,3,4,"banana" ,{name:"abhinav"},[10,20,30]];
+console.log(mixed[1,2]);
+// console.log(mixed[2]);
+console.log(mixed[2].name);
+console.log(mixed[4][2]);
+
+let flen=fruits.length;
+let text=" ";
+for (let i=0; i<flen;i++){
+    text +=``
+}
