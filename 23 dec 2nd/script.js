578a3d5603a3a649a47090b899976fc1eeb288d6
AbhinavGalar <abhinavgalar442@gmail.com>
2024-12-23 23:57:34 +0530

    dec23

---
+++ 23 dec 2nd/script.js
@@ -0,0 +1,18 @@
+const num=[1,3,4,5,6,4,5,4];
+const num1=[30,5,6,7,];
+console.log(num1);
+
+const array=[]
+    array[0]="happy",
+    array[1]="new",
+    array[2]="year",
+    console.log(array);
+
+    const fruits =["mango","kiwi","grapes","black dog","bacardi","old monk"];
+    const mixed=[1,3,2,32,"hello",{name:"abhinav"},[10,20,30]];
+    console.log(mixed(2,1));
+
+
+    // push method add elemnts in the begnings of the array
+    const name =["mango","kiwi","grapes","black dog","bacardi","old monk"];
+    name.pop()
